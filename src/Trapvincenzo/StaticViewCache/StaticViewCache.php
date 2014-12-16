c7aedb345ecfd6b6a79fac2274150066105b5cd7
Vincenzo Trapani <vincenzo.trapani@dservicemedia.it>
2014-12-16 12:21:15 +0100

    Indentation

--- src/Trapvincenzo/StaticViewCache/StaticViewCache.php
+++ src/Trapvincenzo/StaticViewCache/StaticViewCache.php
@@ -7,117 +7,116 @@ use Illuminate\View\FileViewFinder;
  **/
 class StaticViewCache extends FileViewFinder
 {
-	/**
-	 * Gets the html for the requested view
-	 *
-	 * @param string $name
-	 * @param string $id
-	 * @param array $data
-	 * @return string
-	 */
-	public function render($name, $id, $data)
-	{	
+    /**
+     * Gets the html for the requested view
+     *
+     * @param string $name
+     * @param string $id
+     * @param array $data
+     * @return string
+     */
+    public function render($name, $id, $data)
+    {   
+        $directory = $this->getPath();
 
-		$directory = $this->getPath();
-
-		if (!$this->files->isDirectory($directory)) {
-			$this->files->makeDirectory($directory);
-		}
+        if (!$this->files->isDirectory($directory)) {
+            $this->files->makeDirectory($directory);
+        }
 
 
-		$filename = $this->getFilename($name, $id);
+        $filename = $this->getFilename($name, $id);
 
-		return ($this->isExpired($name, $id))
-			? ($this->save($name, $filename, $data))
-			: ($this->getContent($filename));
-	}
+        return ($this->isExpired($name, $id))
+            ? ($this->save($name, $filename, $data))
+            : ($this->getContent($filename));
+    }
 
-	/**
-	 * Checks if the view is expired
-	 *
-	 * @param string $name
-	 * @param string $id
-	 * @return bool
-	 */
-	private function isExpired($name, $id)
-	{
-		$filename = $this->getFilename($name, $id);
+    /**
+     * Checks if the view is expired
+     *
+     * @param string $name
+     * @param string $id
+     * @return bool
+     */
+    private function isExpired($name, $id)
+    {
+        $filename = $this->getFilename($name, $id);
 
-		if (!$this->files->exists($filename) || \Config::get('app.debug', false) === true)
-		{
-			return true;
-		}
+        if (!$this->files->exists($filename) || \Config::get('app.debug', false) === true)
+        {
+            return true;
+        }
 
-		$realLastModified = $this->files->lastModified($this->find($name));
-		$cacheLastModified = $this->files->lastModified($filename);
+        $realLastModified = $this->files->lastModified($this->find($name));
+        $cacheLastModified = $this->files->lastModified($filename);
 
-		return $realLastModified > $cacheLastModified;
-	}
+        return $realLastModified > $cacheLastModified;
+    }
 
-	/**
-	 * Saves the view in cache
-	 *
-	 * @param string $name
-	 * @param string $filename
-	 * @param array $data
-	 * @return string
-	 */
-	public function save($name, $filename, $data)
-	{
-		$content = \View::make($name)->with($data)->render();
+    /**
+     * Saves the view in cache
+     *
+     * @param string $name
+     * @param string $filename
+     * @param array $data
+     * @return string
+     */
+    public function save($name, $filename, $data)
+    {
+        $content = \View::make($name)->with($data)->render();
 
-		$this->files->put($filename, $content);
+        $this->files->put($filename, $content);
 
-		return $content;
-	}
+        return $content;
+    }
 
-	/**
-	 * Gets the content for the cached view
-	 *
-	 * @param string $filename
-	 * @return string
-	 */
-	private function getContent($filename)
-	{
-		return $this->files->get($filename);
-	}
+    /**
+     * Gets the content for the cached view
+     *
+     * @param string $filename
+     * @return string
+     */
+    private function getContent($filename)
+    {
+        return $this->files->get($filename);
+    }
 
-	/**
-	 * Gets the cache path
-	 * 
-	 * @return string
-	 */
-	private function getPath()
-	{
-		return storage_path(). '/views/static/';
-	}
+    /**
+     * Gets the cache path
+     * 
+     * @return string
+     */
+    private function getPath()
+    {
+        return storage_path(). '/views/static/';
+    }
 
-	/**
-	 * Flushs the static cache
-	 *
-	 * @return bool
-	 */
-	public function flush()
-	{	
-		if ($this->files->isDirectory($this->getPath())) {
-			$this->files->cleanDirectory($this->getPath());
-		} else {
-			return false;
-		}
+    /**
+     * Flushs the static cache
+     *
+     * @return bool
+     */
+    public function flush()
+    {   
+        if ($this->files->isDirectory($this->getPath())) {
+            $this->files->cleanDirectory($this->getPath());
+        } else {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Gets the static filename
-	 * 
-	 * @param string $name
-	 * @param string $id
-	 * @return string
-	 */
-	private function getFilename($name, $id)
-	{
-		return  $this->getPath() . md5($name . $id);
-	}
+    /**
+     * Gets the static filename
+     * 
+     * @param string $name
+     * @param string $id
+     * @return string
+     */
+    private function getFilename($name, $id)
+    {
+        return  $this->getPath() . md5($name . $id);
+    }
 
 }
